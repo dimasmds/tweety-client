fde0740155af0b0043c8dab151a8f1662c5473f4
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-24 17:23:43 +0700

    Pass test

---
+++ core/lib/infrastucture/tweets/redux/tweetAction.ts
@@ -0,0 +1,16 @@
+import Tweet from '../../../shared/ValueObject/Tweet';
+
+const TweetAction = {
+  RECEIVE_TWEETS: 'RECEIVE_TWEETS',
+  ADD_TWEETS: 'ADD_TWEETS',
+  RETWEET: 'RETWEET',
+  LIKE_TWEET: 'LIKE_TWEET',
+  DELETE_TWEET: 'DELETE_TWEET',
+};
+
+const receiveTweetsAction = (tweets: Array<Tweet>) => ({
+  type: TweetAction.RECEIVE_TWEETS,
+  tweets,
+});
+
+export { TweetAction, receiveTweetsAction };
