ddcc0ef44fd52612db332ee1227b5886f859d588
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-24 23:10:46 +0700

    pass test

--- core/lib/tweets/redux/reducers/tweets.ts
+++ core/lib/tweets/redux/reducers/tweets.ts
@@ -5,6 +5,8 @@ export const tweetsReducers = (state: Array<Tweet> = [], action: any) => {
   switch (action.type) {
     case TweetAction.RECEIVE_TWEETS:
       return action.tweets;
+    case TweetAction.ADD_TWEETS:
+      return [...state, action.newTweet];
     default:
       return state;
   }
