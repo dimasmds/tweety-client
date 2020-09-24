70883888ee5f5b6cb802a63aceca2f048fb7b44b
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-24 22:24:04 +0700

    Add more case

--- core/__test__/tweets/redux/reducers/tweets.test.ts
+++ core/__test__/tweets/redux/reducers/tweets.test.ts
@@ -1,7 +1,17 @@
 import { tweetsReducers } from '../../../../lib/tweets/redux/reducers';
+import { TweetAction } from '../../../../lib/tweets/redux/actions';
+import { mockedTweets } from '../../__mocks__/tweet';
 
 describe('Tweets Reducer', () => {
   it('should return the initial state', () => {
     expect(tweetsReducers(undefined, {})).toEqual([]);
+  });
+
+  it('should handle RECEIVE_DATA correctly', () => {
+    expect(tweetsReducers([], {
+      type: TweetAction.RECEIVE_TWEETS,
+      tweets: mockedTweets,
+    }))
+      .toEqual(mockedTweets);
   });
 });
