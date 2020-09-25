a1c63759b30545393efcad45ec12bbf13c4f0c22
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-25 20:04:54 +0700

    Adding authReducer to store

--- core/lib/frameworks/redux/store/store.ts
+++ core/lib/frameworks/redux/store/store.ts
@@ -3,11 +3,13 @@ import { tweetsReducers } from '../../../_tweets/adapters/redux/reducers';
 import { middlewares } from '../middlewares';
 import { loadingReducer } from '../../../_shared/loading/adapters/redux';
 import { usersReducer } from '../../../_users/adapters/redux/reducers';
+import { authReducer } from '../../../_authentication/adapters/redux/reducers';
 
 const rootReducer = {
   tweets: tweetsReducers,
   users: usersReducer,
   loading: loadingReducer,
+  auth: authReducer,
 };
 
 export const store = createStore(
