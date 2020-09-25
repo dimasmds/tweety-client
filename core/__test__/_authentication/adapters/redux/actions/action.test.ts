62ef02e538529b2a56573cea4775e4f837dfdd70
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-25 17:46:15 +0700

    Add Authentication Action test

---
+++ core/__test__/_authentication/adapters/redux/actions/action.test.ts
@@ -0,0 +1,9 @@
+describe('Authentication Action', () => {
+  it('should create an action for log in correctly', () => {
+    const expectedId = 'dimasmds';
+    expect(logInAction(expectedId)).toEqual({
+      type: AuthenticationAction.LOG_IN,
+      id: expectedId,
+    });
+  });
+});
