95d4e16faa101cad80c44672ac6f66d36b6363b1
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-25 16:31:45 +0700

    Add initial users action thunks test

--- core/__test__/_users/adapters/actions/action.test.ts
+++ core/__test__/_users/adapters/actions/action.test.ts
@@ -1,5 +1,13 @@
+import fetchMock from 'fetch-mock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import { mockedUsers } from '../../__mocks__/user';
 import { receiveUsersAction } from '../../../../lib/_users/adapters/redux/actions/users';
+import { EndpointAPI } from '../../../../lib/config';
+import { LoadingAction } from '../../../../lib/_shared/loading/adapters/redux';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('Users Action', () => {
   it('should create an action to receive users correctly', () => {
@@ -11,5 +19,21 @@ describe('Users Action', () => {
     };
 
     expect(receiveUsersAction(users)).toEqual(expectedAction);
+  });
+
+  it('should create RECEIVE_USERS action when fetching users from API', async () => {
+    fetchMock.postOnce(EndpointAPI.getAllUsers, {
+      body: mockedUsers,
+    });
+
+    const expectedActions = [
+      { type: LoadingAction.LOADING },
+      { type: 'RECEIVE_USERS', users: mockedUsers },
+      { type: LoadingAction.READY },
+    ];
+
+    const store = mockStore([]);
+    await store.dispatch(handleInitialUsers());
+    expect(store.getActions()).toEqual(expectedActions);
   });
 });
--- core/lib/config/EndpointAPI.ts
+++ core/lib/config/EndpointAPI.ts
@@ -2,4 +2,5 @@ export const BASE_URL = 'https://vast-falls-98498.herokuapp.com';
 export const EndpointAPI = {
   getAllTweets: `${BASE_URL}/tweets`,
   addNewTweet: `${BASE_URL}/add`,
+  getAllUsers: `${BASE_URL}/users`,
 };
