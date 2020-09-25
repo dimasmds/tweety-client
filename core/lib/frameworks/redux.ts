eabd7616380e3955a9893ea26e723791c8657fa3
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-25 13:43:58 +0700

    Add loading to store

--- core/lib/frameworks/redux.ts
+++ core/lib/frameworks/redux.ts
@@ -1,9 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 import { tweetsReducers } from '../_tweets/adapters/redux/reducers';
 import { middlewares } from '../_tweets/adapters/redux/middlewares';
+import { loadingReducer } from '../_shared/loading/adapters/redux';
 
 const rootReducer = {
   tweets: tweetsReducers,
+  loading: loadingReducer,
 };
 
 export const store = createStore(
