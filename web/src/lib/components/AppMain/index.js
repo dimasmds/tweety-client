6552c9050cd5c96717db87648a982566f84afff2
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-25 23:56:26 +0700

    Catch route if not authed

--- web/src/lib/components/AppMain/index.js
+++ web/src/lib/components/AppMain/index.js
@@ -1,9 +1,35 @@
 import { outlet } from 'lit-element-router';
 import { html } from 'lit-html';
-import { LitElement } from 'lit-element';
+import { connect } from 'pwa-helpers';
+import { store } from 'Core/lib/frameworks/redux/store';
+import CommonElement from '../__base__/CommonElement';
+import { handleLogin } from '../../../../../core/lib/_authentication/adapters/redux/actions';
 
-class AppMain extends outlet(LitElement) {
+class AppMain extends connect(store)(outlet(CommonElement)) {
+  static get properties() {
+    return {
+      _auth: { type: String },
+    };
+  }
+
+  stateChanged({ auth }) {
+    this._auth = auth;
+  }
+
+  constructor() {
+    super();
+    this._auth = null;
+  }
+
+  firstUpdated(_changedProperties) {
+    super.firstUpdated(_changedProperties);
+    store.dispatch(handleLogin('dimasmds', 'qwerty123'));
+  }
+
   render() {
+    if (!this._auth) {
+      return html`<h1>Belum login</h1>`;
+    }
     return html`
       <slot></slot>
     `;
