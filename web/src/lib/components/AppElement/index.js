0b53205c11e757e52691e8503831931d30dcf7ec
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-25 13:16:03 +0700

    Use redux in component

--- web/src/lib/components/AppElement/index.js
+++ web/src/lib/components/AppElement/index.js
@@ -1,8 +1,11 @@
 import { html } from 'lit-html';
+import { connect } from 'pwa-helpers';
 import CommonElement from '../__base__/CommonElement';
 import style from './style.scss';
+import { store } from '../../../../../core/lib/frameworks';
+import { handleInitialTweets } from '../../../../../core/lib/tweets/adapters/redux/actions';
 
-class AppElement extends CommonElement {
+class AppElement extends connect(store)(CommonElement) {
   static get styles() {
     return [...super.styles, style];
   }
@@ -10,6 +13,7 @@ class AppElement extends CommonElement {
   static get properties() {
     return {
       _title: { type: String },
+      _tweets: { type: Array },
     };
   }
 
@@ -17,12 +21,30 @@ class AppElement extends CommonElement {
     super();
     // eslint-disable-next-line no-underscore-dangle
     this._title = '';
+    this._tweets = [];
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    store.dispatch(handleInitialTweets());
+  }
+
+  stateChanged({ tweets }) {
+    this._tweets = tweets;
   }
 
   render() {
+    if (!this._tweets.length) {
+      return html`
+        <div class="app">
+          <h1>${this._title}</h1>
+        </div>
+    `;
+    }
+
     return html`
       <div class="app">
-        <h1>${this._title}</h1>
+        <h1>${JSON.stringify(this._tweets)}</h1>
       </div>
     `;
   }
