64dc523cf01473ab4e366fbfb46807b15bd74a70
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-25 17:33:58 +0700

    Adding more case

--- core/__test__/_users/adapters/reducers/users.test.ts
+++ core/__test__/_users/adapters/reducers/users.test.ts
@@ -1,5 +1,15 @@
+import { usersReducer } from '../../../../lib/_users/adapters/redux/reducer';
+import { mockedUsers } from '../../__mocks__/user';
+
 describe('Users Reducers', () => {
   it('should return initial data correctly', () => {
-    expect(userReducer(undefined, {})).toEqual([]);
+    expect(usersReducer(undefined, {})).toEqual([]);
+  });
+
+  it('should handle receive users action correctly', () => {
+    expect(usersReducer([], {
+      type: 'RECEIVE_USERS',
+      users: mockedUsers,
+    })).toEqual(mockedUsers);
   });
 });
