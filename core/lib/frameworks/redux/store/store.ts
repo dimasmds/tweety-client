6099f2e3dca8cab7b1b82590e2a3e612769c8f6a
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-25 17:37:49 +0700

    Add userReducer to store

--- core/lib/frameworks/redux/store/store.ts
+++ core/lib/frameworks/redux/store/store.ts
@@ -2,9 +2,11 @@ import { createStore, combineReducers } from 'redux';
 import { tweetsReducers } from '../../../_tweets/adapters/redux/reducers';
 import { middlewares } from '../middlewares';
 import { loadingReducer } from '../../../_shared/loading/adapters/redux';
+import { usersReducer } from '../../../_users/adapters/redux/reducer';
 
 const rootReducer = {
   tweets: tweetsReducers,
+  users: usersReducer,
   loading: loadingReducer,
 };
 
