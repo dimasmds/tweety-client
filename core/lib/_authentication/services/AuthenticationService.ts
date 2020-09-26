77ef13a7462c2c0304e81a823f10d6132bc7ae34
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-26 14:30:47 +0700

    pass test

--- core/lib/_authentication/services/AuthenticationService.ts
+++ core/lib/_authentication/services/AuthenticationService.ts
@@ -1,9 +1,20 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import { LogInService } from '../useCases';
 import { Authentication } from '../entities';
 import { EndpointAPI } from '../../config';
 
 export class AuthenticationService implements LogInService {
   async logIn(username: string, password: string): Promise<Authentication> {
+    try {
+      const authentication: Authentication = await this._requestLoginToAPI(username, password);
+      await this._saveIdToStorage(authentication.userId);
+      return authentication;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  private async _requestLoginToAPI(username: string, password: string) {
     try {
       const response = await fetch(EndpointAPI.logIn, {
         method: 'POST',
@@ -12,8 +23,15 @@ export class AuthenticationService implements LogInService {
           'Content-Type': 'application/json',
         },
       });
-      const { success, message, userId }: Authentication = await response.json();
-      return { success, message, userId };
+      return await response.json();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  private async _saveIdToStorage(id: string) {
+    try {
+      await AsyncStorage.setItem('AUTH_ID', id);
     } catch (error) {
       throw new Error(error);
     }
