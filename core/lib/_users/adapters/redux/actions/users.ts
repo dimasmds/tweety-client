bede2a371551ac7f90527e21b842379941c0526c
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-26 22:23:26 +0700

    pass test

--- core/lib/_users/adapters/redux/actions/users.ts
+++ core/lib/_users/adapters/redux/actions/users.ts
@@ -1,7 +1,8 @@
-import { User } from '../../../entities';
+import { NewUser, User } from '../../../entities';
 import { setLoadingAction, setReadyAction } from '../../../../_shared/loading/adapters/redux';
 import { UserService } from '../../../services';
-import { UserGetterServiceInteractor } from '../../../useCase';
+import { RegisterUserServiceInteractor, UserGetterServiceInteractor } from '../../../useCase';
+import { addErrorAction } from '../../../../_shared/error/adapters/redux';
 
 export const UserAction = {
   RECEIVE_USERS: 'RECEIVE_USERS',
@@ -19,5 +20,22 @@ export const handleInitialUsers = () => async (dispatch: any) => {
 
   const users = await interactor.getAllUsers();
   dispatch(receiveUsersAction(users));
+  dispatch(setReadyAction());
+};
+
+export const handleRegisterUser = (newUser: NewUser) => async (dispatch: any) => {
+  dispatch(setLoadingAction());
+  const service = new UserService();
+  const interactor = new RegisterUserServiceInteractor(service);
+
+  try {
+    await interactor.registerUser(newUser);
+  } catch ({ message }) {
+    dispatch(addErrorAction({
+      userMessage: message || 'Failed to register, try again!',
+      originalMessage: message,
+    }));
+  }
+
   dispatch(setReadyAction());
 };
