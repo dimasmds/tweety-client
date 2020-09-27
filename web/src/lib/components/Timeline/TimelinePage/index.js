c1472e6c88a1422adefb68fd93082217b67eea4a
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-09-27 15:40:35 +0700

    Initial Timeline page

---
+++ web/src/lib/components/Timeline/TimelinePage/index.js
@@ -0,0 +1,20 @@
+import { html } from 'lit-html';
+import CommonElement from '../../__base__/CommonElement';
+
+import style from './style.scss';
+
+class TimelinePage extends CommonElement {
+  static get styles() {
+    return [...super.styles, style];
+  }
+
+  render() {
+    return html`
+      <div class="timeline-page">
+        <app-bar></app-bar>
+      </div>
+    `;
+  }
+}
+
+customElements.define('timeline-page', TimelinePage);
